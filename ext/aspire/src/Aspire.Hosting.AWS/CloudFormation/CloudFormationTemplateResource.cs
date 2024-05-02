60865d6af4233d21478b90980a6b05e70930efb7
Vincent Lesierse <vinles@amazon.nl>
2024-05-02 17:12:55 +0200

    Include Aspire

---
+++ ext/aspire/src/Aspire.Hosting.AWS/CloudFormation/CloudFormationTemplateResource.cs
@@ -0,0 +1,57 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Aspire.Hosting.Publishing;
+using Aspire.Hosting.Utils;
+
+namespace Aspire.Hosting.AWS.CloudFormation;
+
+/// <inheritdoc/>
+internal sealed class CloudFormationTemplateResource(string name, string templatePath) : CloudFormationResource(name), ICloudFormationTemplateResource, ICloudFormationTemplateProvider
+{
+    public IDictionary<string, string> CloudFormationParameters { get; } = new Dictionary<string, string>();
+
+    /// <inheritdoc/>
+    public string TemplatePath { get; } = templatePath;
+
+    /// <inheritdoc/>
+    public string? RoleArn { get; set; }
+
+    /// <inheritdoc/>
+    public int StackPollingInterval { get; set; } = 3;
+
+    /// <inheritdoc/>
+    public bool DisableDiffCheck { get; set; }
+
+    /// <inheritdoc/>
+    public IList<string> DisabledCapabilities { get; } = [];
+
+    /// <inheritdoc/>
+    public ICloudFormationTemplateResource AddParameter(string parameterName, string parameterValue)
+    {
+        CloudFormationParameters[parameterName] = parameterValue;
+        return this;
+    }
+
+    internal void WriteToManifest(ManifestPublishingContext context)
+    {
+        context.Writer.WriteString("type", "aws.cloudformation.template.v0");
+        context.Writer.TryWriteString("stack-name", Name);
+        context.Writer.TryWriteString("template-path", context.GetManifestRelativePath(TemplatePath));
+
+        context.Writer.WritePropertyName("references");
+        context.Writer.WriteStartArray();
+        foreach (var cloudFormationResource in Annotations.OfType<CloudFormationReferenceAnnotation>())
+        {
+            context.Writer.WriteStartObject();
+            context.Writer.WriteString("target-resource", cloudFormationResource.TargetResource);
+            context.Writer.WriteEndObject();
+        }
+        context.Writer.WriteEndArray();
+    }
+
+    string ICloudFormationTemplateProvider.StackName => Name;
+
+    Task<string> ICloudFormationTemplateProvider.GetCloudFormationTemplate(CancellationToken cancellationToken)
+        => File.ReadAllTextAsync(TemplatePath, cancellationToken);
+}
